642effdea8c0feb03aa7780604659f31b23ca3c0
Silviane Bezerra <silvianeraquel@gmail.com>
2022-05-13 14:40:12 -0300

    Front-end and Back-end comunication by fetch

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
+import React from "react";
+import { useEffect,useState } from "react";
+
 function App() {
+
+  const [data, setData] = useState(null);
+
+  useEffect((()=>{
+    fetch("/api")
+   .then((res) => res.json())
+   .then((data) => setData(data.message));
+   console.log(data)
+   
+  }),[]);
+
   return (
     <div className="App">
-      Client
+      <p>{!data ? "loading...": data}</p>
     </div>
   );
 }
