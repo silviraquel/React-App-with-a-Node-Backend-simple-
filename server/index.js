61dfafab9b31a1794045b39193012b10afeec6a4
Silviane Bezerra <silvianeraquel@gmail.com>
2022-05-13 10:20:09 -0300

    Changed the PORT

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 // npm init to add the package.json in the project
 const express = require("express");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
